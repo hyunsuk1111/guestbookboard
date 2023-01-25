ae1315450ad9cf71e28ebac5e2251baddb8d8a57
hyunsuk1111 <epsxkf@gmail.com>
2023-01-25 20:24:41 +0900

    레포지토리 수정 및 테스트케이스 생성

--- board/src/main/java/org/zerock/board/repository/ReplyRepository.java
+++ board/src/main/java/org/zerock/board/repository/ReplyRepository.java
@@ -1,4 +1,7 @@
 package org.zerock.board.repository;
 
-public interface ReplyRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.zerock.board.entity.Reply;
+
+public interface ReplyRepository extends JpaRepository<Reply, Long> {
 }
---
+++ board/src/test/java/org/zerock/board/repository/BoardRepositoryTests.java
@@ -0,0 +1,9 @@
+package org.zerock.board.repository;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class BoardRepositoryTests {
+
+}
