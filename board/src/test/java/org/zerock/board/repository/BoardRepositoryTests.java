e6708897e873dec1b3500a0d3462a9f1ea5f2e4c
hyunsuk1111 <epsxkf@gmail.com>
2023-02-26 20:14:04 +0900

    테스트케이스 수정중

--- board/src/test/java/org/zerock/board/repository/BoardRepositoryTests.java
+++ board/src/test/java/org/zerock/board/repository/BoardRepositoryTests.java
@@ -4,6 +4,10 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.transaction.annotation.Transactional;
 import org.zerock.board.entity.Board;
 import org.zerock.board.entity.Member;
@@ -49,7 +53,7 @@ public class BoardRepositoryTests {
     }
 
     @Test
-    @DisplayName("테스트_작성자가져오기")
+    @DisplayName("작성자가져오기")
     public void testReadWithWriter(){
         Object result = boardRepository.getBoardWithWriter(100L);
 
@@ -57,14 +61,37 @@ public class BoardRepositoryTests {
     }
 
     @Test
-    @DisplayName("테스트_게시글,댓글가져오기")
+    @DisplayName("게시글,댓글가져오기")
     public void testGetBoardWithReply(){
         List<Object[]> result = boardRepository.getBoardWithReply(100L);
 
         for(Object[] arr : result){
             System.out.println(Arrays.toString(arr));
         }
+    }
 
+    @Test
+    @DisplayName("목록화면테스트")
+    public void testWithReplyCount(){
+        Pageable pageable = PageRequest.of(0, 10, Sort.by("bno").descending());
+
+        Page<Object[]> result = boardRepository.getBoardWithReplyCount(pageable);
+
+        result.get().forEach(row -> {
+            Object[] arr = (Object[]) row;
+
+            System.out.println(Arrays.toString(arr));
+        });
+    }
+
+    @Test
+    @DisplayName("특정게시물")
+    public void testRead3(){
+        Object result = boardRepository.getBoardByBno(100L);
+
+        Object[] arr = (Object[]) result;
+
+        System.out.println(Arrays.toString(arr));
     }
 
 
